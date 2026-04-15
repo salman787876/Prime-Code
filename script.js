30230a8fb797ae8eec1a0c07c8d0ff6234d646da
salman787876 <salman00778876@gmail.com>
2026-04-15 20:15:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function generateOrderID() {
+  return "PRIME-" + Math.floor(100000 + Math.random() * 900000);
+}
 
+function createOrder() {
+  const name = document.getElementById("name").value;
+  const service = document.getElementById("service").value;
+
+  if (!name) {
+    alert("اكتب اسمك");
+    return;
+  }
+
+  const orderId = generateOrderID();
+
+  // تخزين مؤقت (محلي)
+  const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+  orders.push({
+    name: name,
+    service: service,
+    orderId: orderId,
+    status: "pending"
+  });
+
+  localStorage.setItem("orders", JSON.stringify(orders));
+
+  document.getElementById("result").innerHTML =
+    "✅ تم إنشاء الطلب<br>📦 رقم الطلب: " + orderId;
+}
